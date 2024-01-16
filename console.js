40da23f797dd56e1940fb5041ffd15d75115fc42
daenur <daenur@outlook.com>
2024-01-16 16:28:27 +0200

    Create console.js

---
+++ console.js
@@ -0,0 +1,2 @@
+localStorage.setItem('gameState','{"grid":{"size":4,"cells":[[{"position":{"x":0,"y":0},"value":32768},null,null,null],[{"position":{"x":1,"y":0},"value":2},null,null,null],[null,null,null,null],[{"position":{"x":3,"y":0},"value":2},null,null,null]]},"score":453304,"over":false,"won":false,"keepPlaying":false}');
+location.reload();
